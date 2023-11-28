86262768889bb217c620576d9a2d655e232a030b
Chao FANG <scycf1@nottingham.ac.uk>
2023-11-28 00:06:30 +0000

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -39,13 +39,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Map<String, String> parseContent(String content) {
     Map<String, String> resultMap = {};
-    var entries = content.split('\n'); // 使用逗号分割字符串
+    var entries = content.split('\n');
 
     for (var entry in entries) {
-      var keyValue = entry.split(':'); // 分割键和值
+      var keyValue = entry.split(':');
       if (keyValue.length == 2) {
-        var key = keyValue[0].trim(); // 清除空格
-        var value = keyValue[1].trim(); // 清除空格
+        var key = keyValue[0].trim();
+        var value = keyValue[1].trim();
         resultMap[key] = value;
       }
     }
@@ -96,13 +96,9 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     String response = await sendToOpenAI();
     setState(() {
-      // 解析响应 JSON 字符串
       var jsonResponse = jsonDecode(response);
-      // 假设 "choices" 数组的第一个元素的 "message" 对象包含 "content" 字段
       var contentString = jsonResponse['choices'][0]['message']['content'];
-      // 将 "content" 字段的字符串转换为 Map
       Map<String, String> contentMap = parseContent(contentString);
-      // 现在 contentMap 包含了转换后的哈希表，可以根据需要使用它
       _response = contentMap.entries
           .map((entry) => '${entry.key}: ${entry.value}')
           .join(', ');
@@ -111,39 +107,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
