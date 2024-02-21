e819d2d09458fc1646275bae34a1a7a50895b1d5
MKoli <efymk3@nottingham.ac.uk>
2024-02-21 00:28:10 +0000

    UI: new settings dropdowns(WIP)

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -22,93 +22,206 @@ class _SettingsScreenState extends State<SettingsScreen> {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: <Widget>[
-            DropdownButtonFormField<int>(
-              value: _selectedDifficulty,
-              items: [
-                DropdownMenuItem<int>(
-                  value: 1,
-                  child: Text('1 (Easy)'),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.white,
+              ),
+              child: DropdownButtonFormField<int>(
+                value: _selectedDifficulty,
+                items: [
+                  DropdownMenuItem<int>(
+                    value: 1,
+                    child: Text(
+                      '1 (Easy)',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 2,
+                    child: Text(
+                      '2',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 3,
+                    child: Text(
+                      '3',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 4,
+                    child: Text(
+                      '4',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 5,
+                    child: Text(
+                      '5 (Hard)',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _selectedDifficulty = value!;
+                  });
+                },
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  labelText: 'Difficulty',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24, // Adjust label font size here
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
                 ),
-                DropdownMenuItem<int>(
-                  value: 2,
-                  child: Text('2'),
-                ),
-                DropdownMenuItem<int>(
-                  value: 3,
-                  child: Text('3'),
-                ),
-                DropdownMenuItem<int>(
-                  value: 4,
-                  child: Text('4'),
-                ),
-                DropdownMenuItem<int>(
-                  value: 5,
-                  child: Text('5 (Hard)'),
-                ),
-              ],
-              onChanged: (value) {
-                setState(() {
-                  _selectedDifficulty = value!;
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Difficulty',
-                labelStyle:
-                    TextStyle(color: Colors.white), // Set label color to white
               ),
             ),
-            DropdownButtonFormField<String>(
-              value: _selectedCookingTime,
-              items: [
-                '30 min',
-                '1 h',
-                '2 h',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  _selectedCookingTime = value!;
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Cooking Time',
-                labelStyle:
-                    TextStyle(color: Colors.white), // Set label color to white
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.white,
+              ),
+              child: DropdownButtonFormField<String>(
+                value: _selectedCookingTime,
+                items: [
+                  '30 min',
+                  '1 h',
+                  '2 h',
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCookingTime = value!;
+                  });
+                },
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  labelText: 'Cooking Time',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24, // Adjust label font size here
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
               ),
             ),
-            DropdownButtonFormField<int>(
-              value: _selectedPortionSize,
-              items: [
-                DropdownMenuItem<int>(
-                  value: 1,
-                  child: Text('For 1 person'),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Colors.white,
+              ),
+              child: DropdownButtonFormField<int>(
+                value: _selectedPortionSize,
+                items: [
+                  DropdownMenuItem<int>(
+                    value: 1,
+                    child: Text(
+                      'For 1 person',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 2,
+                    child: Text(
+                      'For 2 people',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 4,
+                    child: Text(
+                      'For 4 people',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                  DropdownMenuItem<int>(
+                    value: 6,
+                    child: Text(
+                      'For 6 people',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18, // Adjust font size here
+                      ),
+                    ),
+                  ),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _selectedPortionSize = value!;
+                  });
+                },
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  labelText: 'Portion Size',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24, // Adjust label font size here
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
                 ),
-                DropdownMenuItem<int>(
-                  value: 2,
-                  child: Text('For 2 people'),
-                ),
-                DropdownMenuItem<int>(
-                  value: 4,
-                  child: Text('For 4 people'),
-                ),
-                DropdownMenuItem<int>(
-                  value: 6,
-                  child: Text('For 6 people'),
-                ),
-              ],
-              onChanged: (value) {
-                setState(() {
-                  _selectedPortionSize = value!;
-                });
-              },
-              decoration: InputDecoration(
-                labelText: 'Portion Size',
-                labelStyle:
-                    TextStyle(color: Colors.white), // Set label color to white
               ),
             ),
           ],
