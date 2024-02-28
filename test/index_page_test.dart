b038fd49765016594944a39e7fb5e2f20ba8645c
psyec4 <psyec4@nottingham.ac.uk>
2024-02-28 23:47:07 +0000

    Fix: test cases for index page

--- test/index_page_test.dart
+++ test/index_page_test.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:food_ai_app/main.dart';
+import 'package:food_ai_app/Pages/index_page.dart';
 
 // L - Unit tests for the index page class.
 void main() {
   testWidgets('Test to see when the application is open that it opens the index page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body: IndexPage())));
 
     // Verify that we're on the index page.
     expect(find.text('Lets find the food you love'), findsOneWidget);
@@ -15,7 +14,7 @@ void main() {
 
   testWidgets('User clicks on the login button and is redirected to the login page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body: IndexPage())));
 
     // Verify that we're on the index page.
     expect(find.text('Lets find the food you love'), findsOneWidget);
@@ -25,13 +24,13 @@ void main() {
     await tester.pumpAndSettle();
 
     // Verify that we're on the login page.
-    expect(find.byKey(ValueKey('usernameField')), findsOneWidget);
-    expect(find.byKey(ValueKey('passwordField')), findsOneWidget);
+    expect(find.byKey(ValueKey('emailTextField')), findsOneWidget);
+    expect(find.byKey(ValueKey('passwordTextField')), findsOneWidget);
   });
 
   testWidgets('User clicks on the sign up button and is redirected to the sign up page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body: IndexPage())));
 
     // Verify that we're on the index page.
     expect(find.text('Lets find the food you love'), findsOneWidget);
@@ -41,9 +40,9 @@ void main() {
     await tester.pumpAndSettle();
 
     // Verify that we're on the sign up page.
-    expect(find.byKey(ValueKey('usernameField')), findsOneWidget);
-    expect(find.byKey(ValueKey('emailField')), findsOneWidget);
-    expect(find.byKey(ValueKey('passwordField')), findsOneWidget);
-    expect(find.byKey(ValueKey('confirmPasswordField')), findsOneWidget);
+    expect(find.byKey(ValueKey('usernameTextField')), findsOneWidget);
+    expect(find.byKey(ValueKey('emailTextField')), findsOneWidget);
+    expect(find.byKey(ValueKey('passwordTextField')), findsOneWidget);
+    expect(find.byKey(ValueKey('confirmPasswordTextField')), findsOneWidget);
   });
 }
