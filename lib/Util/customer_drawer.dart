7d5db8bb933974613ca3b78e818af90babebdff3
Marco Mehta <psymm9@nottingham.ac.uk>
2024-03-13 23:32:05 +0000

    Add user name to menu and change layout

--- lib/Util/customer_drawer.dart
+++ lib/Util/customer_drawer.dart
@@ -3,56 +3,65 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:food_ai_app/LoginPages/index_page.dart';
 import 'package:food_ai_app/main.dart';
 
-import 'package:food_ai_app/LoginPages/home_page.dart'; // Import your main page or any page you navigate to
+import 'package:food_ai_app/LoginPages/home_page.dart';
+
+import 'colours.dart'; // Import your main page or any page you navigate to
 
 class CustomDrawer extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
         children: <Widget>[
           DrawerHeader(
-            child: Text('George Cook', style: TextStyle(color: Colors.white)),
+            child: Text('Signed in as: ${user?.email!}',
+                style: TextStyle(color: Colors.white)),
             decoration: BoxDecoration(
-              color: Colors.blue,
+              color: Colours.primary,
             ),
           ),
-          ListTile(
-            title: Text('Tinder Selection'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
-          ),
-          ListTile(
-            title: Text('Settings'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
-          ),
-          ListTile(
-            title: Text('Preferences'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
-          ),
-          ListTile(
-            title: Text('Health Goals'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  title: Text('Home'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text('Preferences'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text('Health Goals'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text('Settings'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
           ListTile(
             title: Text('Log Out'),
