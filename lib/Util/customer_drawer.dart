b526e5103c3fa7aee843cbb61dc6b4d27e50c7a7
Marco Mehta <psymm9@nottingham.ac.uk>
2024-03-13 23:36:00 +0000

    Drawer design complete

--- lib/Util/customer_drawer.dart
+++ lib/Util/customer_drawer.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:food_ai_app/LoginPages/index_page.dart';
 import 'package:food_ai_app/main.dart';
-
 import 'package:food_ai_app/LoginPages/home_page.dart';
-
-import 'colours.dart'; // Import your main page or any page you navigate to
+import 'package:food_ai_app/Util/colours.dart';
 
 class CustomDrawer extends StatelessWidget {
   final user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: <Widget>[
           DrawerHeader(
-            child: Text('Signed in as: ${user?.email!}',
-                style: TextStyle(color: Colors.white)),
+            child: Text(
+              'Signed in as: ${user?.email ?? 'Not signed in'}',
+              style: TextStyle(color: Colors.white),
+            ),
             decoration: BoxDecoration(
               color: Colours.primary,
             ),
@@ -24,47 +25,32 @@ class CustomDrawer extends StatelessWidget {
           Expanded(
             child: ListView(
               children: <Widget>[
-                ListTile(
-                  title: Text('Home'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  },
+                _buildDrawerItem(
+                  icon: Icons.home,
+                  text: 'Home',
+                  onTap: () => _navigateTo(context, HomePage()),
                 ),
-                ListTile(
-                  title: Text('Preferences'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  },
+                _buildDrawerItem(
+                  icon: Icons.favorite_border,
+                  text: 'Preferences',
+                  onTap: () => _navigateTo(context, HomePage()),
                 ),
-                ListTile(
-                  title: Text('Health Goals'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  },
+                _buildDrawerItem(
+                  icon: Icons.flag,
+                  text: 'Health Goals',
+                  onTap: () => _navigateTo(context, HomePage()),
                 ),
-                ListTile(
-                  title: Text('Settings'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  },
+                _buildDrawerItem(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                  onTap: () => _navigateTo(context, HomePage()),
                 ),
               ],
             ),
           ),
-          ListTile(
-            title: Text('Log Out'),
+          _buildDrawerItem(
+            icon: Icons.logout,
+            text: 'Log Out',
             onTap: () {
               FirebaseAuth.instance.signOut();
               Navigator.pushReplacement(
@@ -75,6 +61,30 @@ class CustomDrawer extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  ListTile _buildDrawerItem(
+      {required IconData icon,
+      required String text,
+      required VoidCallback onTap}) {
+    return ListTile(
+      leading: Icon(icon, color: Colours.primary),
+      title: Text(
+        text,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colours.primary,
+        ),
+      ),
+      onTap: onTap,
+    );
+  }
+
+  void _navigateTo(BuildContext context, Widget page) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => page),
     );
   }
 }
