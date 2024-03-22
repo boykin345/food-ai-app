d85b2e6eb1a21a96f2bcfbc2de483680e7360d15
Mikhail Kolyakin <efymk3@exmail.nottingham.ac.uk>
2024-03-22 15:20:54 +0000

    Revert "Merge branch 'J-gpt' into 'dev'"
    This reverts merge request !35

--- lib/FullRecipeGeneration/gpt_api_recipe.dart
+++ lib/FullRecipeGeneration/gpt_api_recipe.dart
@@ -40,13 +40,17 @@ class GPTRecipeApi {
   }
 
   //function to get a recipe from GPT
-  Future<String> getRecipe(String dishName) async {
+  Future<String> getRecipe(String dishName, bool isImage) async {
     try {
-      String message = " ";
-
-      //instruct the api to return a recipe with name, difficulty cooking time, ingredients and method
-      message =
-          'Tell me how to make $dishName, give me the name of the dish, a difficulty rating out of 10 and estimated cooking time, calories, a list of ingredients and a full method with step-by-step instructions in that order. Insert two newlines after each requested thing';
+      String message = "";
+      if (!isImage) {
+        //instruct the api to return a recipe with name, difficulty cooking time, ingredients and method
+        message =
+            'Tell me how to make $dishName, give me the name of the dish, a difficulty rating out of 10 and estimated cooking time, then give me a list of ingredients and a full method with step-by-step instructions';
+      } else {
+        //instruct the api to get an image of the dish
+        message = "Give me the image address of an image of a $dishName";
+      }
 
       final response = await getChatResponse(message);
       final decodedResponse = jsonDecode(response)
