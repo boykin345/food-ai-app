5b0f7f24a4283bd924e98020fe89119277afebf0
Marco Mehta <psymm9@nottingham.ac.uk>
2024-03-23 22:31:59 +0000

    Add clickable element to home recipes and render in template

--- lib/Entities/recipe_display_template.dart
+++ lib/Entities/recipe_display_template.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:food_ai_app/Entities/recipe.dart';
 
-class RecipeTemplate extends StatefulWidget {
-  @override
-  RecipeTemplateState createState() => RecipeTemplateState();
-}
+class RecipeTemplate extends StatelessWidget {
+  final Recipe recipe;
 
-class RecipeTemplateState extends State<RecipeTemplate> {
+  RecipeTemplate({required this.recipe});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe.recipeName),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text("Name: ${recipe.recipeName}"),
+            Image.network(recipe.imageURL),
+            Text("Category: ${recipe.category}"),
+            Text("Calories: ${recipe.calories}"),
+            Text("Prep Time: ${recipe.prepTime}"),
+            Text("Difficulty: ${recipe.difficulty}"),
+            Text("Instructions: ${recipe.instructions}"),
+          ],
+        ),
+      ),
+    );
   }
 }
