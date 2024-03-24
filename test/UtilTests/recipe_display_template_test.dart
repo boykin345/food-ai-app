3770fc6eaad8164bbe9d1ec8f2163657780ca6e6
Marco Mehta <psymm9@nottingham.ac.uk>
2024-03-24 17:30:52 +0000

    Add tests for recipe template

--- test/UtilTests/recipe_display_template_test.dart
+++ test/UtilTests/recipe_display_template_test.dart
@@ -1,28 +1,43 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:food_ai_app/Entities/recipe.dart';
 import 'package:food_ai_app/Entities/recipe_display_template.dart';
+import 'package:food_ai_app/Util/colours.dart';
+import 'package:food_ai_app/Util/initial_recipes.dart';
+import 'package:mocktail_image_network/mocktail_image_network.dart';
 
 void main() {
   testWidgets('RecipeTemplate golden test', (WidgetTester tester) async {
-    final testRecipe = Recipe(
-      recipeName: "Test Recipe",
-      imageURL: "https://example.com/test_image.jpg",
-      category: "Test Category",
-      calories: 100,
-      prepTime: "10 mins",
-      difficulty: 3,
-      ingredients: ["Ingredient 1", "Ingredient 2"],
-      instructions: "Test instructions.",
-    );
+    final RecipeInitialiser recipeInitialiser = RecipeInitialiser();
+    await mockNetworkImages(() async => tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+            body: RecipeTemplate(recipe: recipeInitialiser.chickenSalad)))));
 
-    await tester.pumpWidget(MaterialApp(
-      home: RecipeTemplate(recipe: testRecipe),
-    ));
+    expect(find.text('Avocado Chicken Salad'), findsOneWidget);
+    expect(find.text('Calories: 500'), findsOneWidget);
+    expect(find.text('Prep Time: 15 minutes'), findsOneWidget);
+    expect(find.text('Difficulty: 2/5'), findsOneWidget);
+    expect(
+        find.text(
+            'In a large bowl, combine all the ingredients and gently stir until the avocado is slightly mashed and all the ingredients are well mixed. Season with salt and pepper to taste. Serve chilled.'),
+        findsOneWidget);
+    expect(find.text('Category: Lunch'), findsOneWidget);
 
-    await expectLater(
-      find.byType(RecipeTemplate),
-      matchesGoldenFile('golden_files/recipe_template.png'),
-    );
+    for (final String ingredient
+        in recipeInitialiser.chickenSalad.ingredients) {
+      final matcher = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is Text &&
+            widget.data!.startsWith('• $ingredient') &&
+            widget.style!.fontSize == 18 &&
+            widget.style!.color == Colours.backgroundOff,
+      );
+
+      expect(matcher, findsOneWidget,
+          reason:
+              "Ingredient '$ingredient' not found or not styled correctly.");
+    }
   });
 }
