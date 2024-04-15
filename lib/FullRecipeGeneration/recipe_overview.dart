0fd7e42a1edfd30bb2497e6245d89a7693a945c6
Elliot Ray <psyer4@exmail.nottingham.ac.uk>
2024-04-15 12:40:54 +0100

    add save recipe functionality

--- lib/FullRecipeGeneration/recipe_overview.dart
+++ lib/FullRecipeGeneration/recipe_overview.dart
@@ -9,8 +9,17 @@ import '../Util/custom_app_bar.dart';
 import '../Util/customer_drawer.dart';
 
 class RecipeOverview extends StatelessWidget {
-  late String dishName;
-  late String imageLink;
+   //store properties of recipe in separate variables such that they can be distinguished for recipe saving
+  late String dishName = " ";
+  String caloriesString = " ";
+  int calories = 0;
+  String cookingTime = " ";
+  String difficultyString = " ";
+  int difficulty = 0;
+  String method = " ";
+  String ingredients = " ";
+  String category = " ";
+  String imageLink;
 
   //create an instance of the gpt class that can be used to make api requests
   GPTRecipeApi gptRecipeApi = GPTRecipeApi('0f91ba9b74344d7699144a8afbeeae2b');
@@ -41,6 +50,74 @@ class RecipeOverview extends StatelessWidget {
     recipe = await gptRecipeApi.getRecipe(dishName, false);
     //recipe = mockRecipeApi.recipeText;
   }
+
+ //method to separate parts of the recipe
+  void splitRecipe(String recipe)
+  {
+    //assign each part a number (in order)
+    //0 - dish name
+    //1 - difficulty
+    //2 - cooking time
+    //3 - category
+    //4 - calories
+    //5 - ingredients
+    //6 - method
+
+    //keep track of these with count variable
+    int count = 0;
+    //int to keep track where to start substrings from
+    int startPoint = 0;
+
+    //loop through string
+    for (int i = 0; i < recipe.length; i++)
+    {
+        //check for double new line
+        if (recipe[i] == '\n')
+        {
+          if (recipe[i + 1] == '\n')
+          {
+            //assign substring to variable
+              switch(count)
+              {
+                case (0):
+                  dishName = recipe.substring(startPoint, i - 1);
+                case (1):
+                  difficultyString = recipe.substring(startPoint, i - 1);
+                case (2):
+                  cookingTime = recipe.substring(startPoint, i - 1);
+                case (3):
+                   category = recipe.substring(startPoint, i - 1);
+                case (4):
+                  caloriesString = recipe.substring(startPoint, i - 1);
+                case (5):
+                  ingredients = recipe.substring(startPoint, i - 1);
+                case (6):
+                  method = recipe.substring(startPoint, i - 1);
+
+              }
+              //increment count and set start point for next substring
+              count++;
+              startPoint = i + 2;
+          }
+        }
+    }
+  }
+
+  //method to save recipe
+  Recipe saveRecipe()
+  {
+    try
+    {
+      difficulty = int.parse(difficultyString);
+      calories = int.parse(caloriesString);
+    }catch (exception)
+    {
+      throw Exception('Could not convert string to int');
+    }
+    final Recipe recipe = Recipe(recipeName: dishName, calories: calories, prepTime: cookingTime, difficulty: difficulty, ingredients: ingredients, instructions: method, category: category, imageURL: imageURL)
+    return recipe;
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -114,6 +191,14 @@ class RecipeOverview extends StatelessWidget {
               )
             ],
           ),
-        ));
+        )),floatingActionButton: FloatingActionButton(
+            onPressed:()
+            {
+              splitRecipe(recipe);
+              saveRecipe();
+            },
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            child: Text("Save"));
   }
 }
